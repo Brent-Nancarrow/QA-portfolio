4cf38f267a00eca422def2271fb4d7a43a2873d6
Brent-Nancarrow <brentsa@hotmail.com>
2026-03-10 14:37:52 +0000

    fixed headless ci

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
+import os
 import pytest
 
 
 @pytest.fixture(scope="session")
 def browser_type_launch_args():
+    is_ci = os.getenv("CI") == "true"
+
     return {
-        "headless": False,
-        "slow_mo": 1000
+        "headless": is_ci,
+        "slow_mo": 0 if is_ci else 500
     }
